a19d06e21f6573d0dac92e472326fbbac1267556
sudhanshu kumar <sudhanshu62026@gmail.com>
2026-07-29 08:54:34 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (11.44%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,24 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-
-        long power = n;   // Use long to handle Integer.MIN_VALUE
-
+     long power = n; 
         if (power < 0) {
             x = 1 / x;
             power = -power;
         }
-
         double result = 1.0;
-
         while (power > 0) {
-            if ((power & 1) == 1) {   // If power is odd
+            if (power % 2 == 1) {   
                 result *= x;
             }
-
             x *= x;
-            power >>= 1;              // power = power / 2
+            power >>= 1;              
         }
-
         return result;
     }
 }
